ab7fbc3ec09b0d8b550acb9922284f8db25004be
michael emmanuel <emmanuelmichaelpk3@gmail.com>
2025-07-11 02:55:52 +0100

    Created user module using NestJS CLI and added it to the codebase

---
+++ src/user/user.entity.ts
@@ -0,0 +1,25 @@
+import { Entity, PrimaryGeneratedColumn, Column, CreateDateColumn, UpdateDateColumn  } from "typeorm";
+
+@Entity("users")
+export class User{
+    @PrimaryGeneratedColumn()
+    id:number;
+
+    @Column({unique:true})
+    username:string;
+
+    @Column({unique:true})
+    email:string;
+
+    @Column()
+    password:string;
+
+    @Column()
+    full_name:string;
+
+    @CreateDateColumn({type: "timestamp"})
+    created_at: Date;
+
+    @UpdateDateColumn({type: "timestamp"})
+    updated_at:Date;
+}
